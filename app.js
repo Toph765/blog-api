e6c824e2e0bb0e142325e470e10f44a4f97ad498
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-06-26 17:24:58 +0800

    setup: authentication

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const passport = require('passport');
+require('./lib/passport.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const blogRouter = require('./routes/blogRouter.js');
-// const authRouter = require('./routes/authRouter.js');
+const authRouter = require('./routes/authRouter.js');
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/posts", blogRouter);
-// app.use("/auth", authRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => { console.log(`Express app listening on port ${PORT}`) });
