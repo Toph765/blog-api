b61cb6c1cfdcc9c91cb404c73121b2c1896549ee
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-06-26 17:30:39 +0800

    setup: get posts route

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const blogRouter = Router();
 const blogController = require('../controllers/blogController');
+const passport = require('passport');
+const auth = require('../lib/auth').auth;
 
-blogRouter.get("/", blogController.blogGet);
+blogRouter.get("/", auth, blogController.blogGet);
 //blogRouter.post("/", blogController.blogPost);
 
 module.exports = blogRouter;
