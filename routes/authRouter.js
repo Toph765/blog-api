2bf397b928ca143fce0066d26d0b71e2317332dd
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-06-26 17:29:15 +0800

    setup: log in route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,5 +3,6 @@ const authRouter = Router();
 const authController = require('../controllers/authController');
 
 authRouter.post("/sign-up", authController.signUpPost);
+authRouter.post("/log-in", authController.logInPost);
 
 module.exports = authRouter;
