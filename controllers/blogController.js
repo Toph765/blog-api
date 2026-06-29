7f3ea09ef1fb1540b13b5abc1aef499153cc00c9
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-06-29 16:16:53 +0800

    feat: add new blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,14 +2,27 @@ const prisma = require('../lib/prisma.js');
 
 async function blogGet(req, res) {
     const posts = await prisma.post.findMany();
-
+    
     return res.json(posts);
 }
 
-/* async function blogPost(req, res) {
-    
-} */
+async function blogPost(req, res) {
+    const {title, content} = req.body;
+
+    const newPost = await prisma.post.create({
+        data: {
+            title,
+            content,
+            published: false,
+            userId: req.user.id,
+            author: req.user.username,
+        }
+    })
+
+    return res.json(newPost)
+}
 
 module.exports = {
     blogGet,
+    blogPost
 }
