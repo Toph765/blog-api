f1a7eb1e7f7dcc7bdf7ff95fcb5431d97618bdce
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-06-30 14:28:43 +0800

    feat: add comment function

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,14 +2,14 @@ const prisma = require('../lib/prisma.js');
 
 async function blogGet(req, res) {
     const posts = await prisma.post.findMany();
-    
+
     return res.json(posts);
 }
 
 async function blogPost(req, res) {
     const {title, content} = req.body;
 
-    const newPost = await prisma.post.create({
+    const newBlog = await prisma.post.create({
         data: {
             title,
             content,
@@ -17,12 +17,28 @@ async function blogPost(req, res) {
             userId: req.user.id,
             author: req.user.username,
         }
-    })
+    });
 
-    return res.json(newPost)
+    return res.json(newBlog);
+}
+
+async function addCommentPost(req, res) {
+    const {content} = req.body;
+
+    const newComment = await prisma.comment.create({
+        data: {
+            content,
+            userId: req.user.id,
+            author: req.user.username,
+            postId: parseInt(req.params.blogId)
+        }
+    });
+
+    return res.json(newComment);
 }
 
 module.exports = {
     blogGet,
-    blogPost
+    blogPost,
+    addCommentPost
 }
