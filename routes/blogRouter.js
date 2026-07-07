d7ebca00b6e5799dab0a7a7b0a9ee85be3778b64
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-07-07 18:05:07 +0800

    setup: update and delete blog, update comment

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const blogRouter = Router();
 const blogController = require('../controllers/blogController');
-const passport = require('passport');
 const auth = require('../lib/auth').auth;
 
 blogRouter.get("/", blogController.blogGet);
 blogRouter.post("/", auth, blogController.blogPost);
+blogRouter.put("/:blogId", auth, blogController.updateBlogPut);
+blogRouter.delete("/:blogId", auth, blogController.deleteBlogDel);
+
 blogRouter.post("/:blogId", auth, blogController.addCommentPost);
+blogRouter.put("/:blogId/comments/:commentId", auth, blogController.updateCommentPost);
 
 module.exports = blogRouter;
