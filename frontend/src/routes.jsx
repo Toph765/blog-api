94b95a9a3f2d159b649130136240498ca17fd4cc
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-08-27 11:25:34 +0800

    setup: routes

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,27 @@
+import Homepage from "./components/Homepage";
+import Blogpost from "./components/Blogposts";
+import LogIn from "./components/Login";
+import SignUp from "./components/SignUp";
+import App from "./App";
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {index: true, element: <Homepage />},
+            {path: "/", element: <Homepage />},
+            {path: "blogposts/:id", element: <Blogpost />}
+        ]
+    },
+    {
+        path: "log-in",
+        element: <LogIn />
+    },
+    {
+        path: "sign-up",
+        element: <SignUp />
+    }
+]
+
+export default routes;
