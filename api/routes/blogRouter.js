5f614cc55a8bfb6e17e944bc276da9ae32ca1e08
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-08-27 11:24:52 +0800

    setup: get all comments, rename function,  get blog route

--- api/routes/blogRouter.js
+++ api/routes/blogRouter.js
@@ -3,11 +3,13 @@ const blogRouter = Router();
 const blogController = require('../controllers/blogController');
 const auth = require('../lib/auth').auth;
 
-blogRouter.get("/", blogController.blogGet);
+blogRouter.get("/", blogController.allBlogGet);
+blogRouter.get("/:blogId", blogController.blogGet);
 blogRouter.post("/", auth, blogController.blogPost);
 blogRouter.put("/:blogId", auth, blogController.updateBlogPut);
 blogRouter.delete("/:blogId", auth, blogController.deleteBlogDel);
 
+blogRouter.get("/:blogId/comments", blogController.getComments);
 blogRouter.post("/:blogId", auth, blogController.addCommentPost);
 blogRouter.put("/:blogId/comments/:commentId", auth, blogController.updateCommentPost);
 blogRouter.delete("/:blogId/comments/:commentId/delete", auth, blogController.commentDel);
