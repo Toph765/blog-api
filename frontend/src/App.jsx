9825d8f8f6c972bf19f8398a9f2d7ca66a4a5d10
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-09-05 10:27:08 +0800

    feat:  log in and sign up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,41 @@
-import { Outlet, Link } from "react-router"
+import { Outlet, Link } from "react-router";
+import { useState } from "react";
 
 const App = () => {
+const [hide, setHide] = useState(false);
+const [user, setUser] = useState({});
+const [disable, setDisable] = useState(true);
+
+const handleSetHide = (boolean) => {
+  setHide(boolean);
+};
+
+const handleSetUser = (user) => {
+  setUser(user);
+};
+
+const handleSetDisable = (boolean) => {
+  setDisable(boolean);
+};
+
   return (
     <>
       <div>
-        <h1>Welcome to the main page!</h1>
-        <Link to="log-in">Log In</Link>
-        <Link to="sign-up">Sign Up</Link>
-        <Outlet />
+          <nav hidden={hide}>
+            {Object.keys(user).length > 0 ? (
+              <div>{user.username}</div>
+            ) : (
+              <>
+                <h1>Welcome to the main page!</h1>
+                <Link to="log-in" onClick={() => handleSetHide(true)}>Log In</Link>
+                <Link to="sign-up" onClick={() => handleSetHide(true)}>Sign Up</Link>
+              </>
+            )}
+        </nav>
+        <Outlet context={{handleSetHide, handleSetUser, handleSetDisable, disable}}/>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
