9e2df32ad6a5275bd70bf5af6ffdfd7ab819798e
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-09-07 19:12:10 +0800

    setup: blog, log in, and sign up route

--- frontend-author/src/routes.jsx
+++ frontend-author/src/routes.jsx
@@ -1,12 +1,20 @@
 import App from "./App";
 import { Homepage } from "./components/Homepage";
+import { Blogpost } from "./components/BlogPost";
+import { LogIn } from "./components/LogIn";
+import { SignUp } from "./components/SignUp";
+import {NewBlog} from "./components/NewBlog";
 
 export const routes = [{
     path: "/",
     element: <App />,
     children: [
         {index: true, element:  <Homepage />},
-        {path: "/homepage", element: <Homepage />}
+        {path: "/homepage", element: <Homepage />},
+        {path: "/blogpost/:id", element: <Blogpost />},
+        {path: "/log-in", element: <LogIn />},
+        {path: "/sign-up", element: <SignUp />},
+        {path: "/new-blog", element: <NewBlog />},
     ]
     
 }]
