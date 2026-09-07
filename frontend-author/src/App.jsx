e2f5fad7f96eb90175347859504bdf86a368cf3f
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-09-07 19:11:45 +0800

    setup: display blogs, log in and sign up

--- frontend-author/src/App.jsx
+++ frontend-author/src/App.jsx
@@ -1,11 +1,37 @@
-import { Outlet } from 'react-router'
+import { Outlet, Link, useNavigate } from 'react-router'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+
+  const handleSetUser = (user) => {
+    setUser(user);
+  };
+
+  const handleNewBlogNav = () => {
+    navigate("/new-blog")
+  }
+
   return (
     <>
+    <nav>
       <h1>Main page</h1>
-      <Outlet />
+      {Object.keys(user).length > 0 ? (
+        <div>{user.username}</div>
+      ) : (
+        <div>
+        <Link to="log-in">Log in</Link>
+        <Link to="sign-up">Sign up</Link>
+      </div>
+      )}
+      <div>
+        <button onClick={handleNewBlogNav}>Create New Blog!</button>
+      </div>
+    </nav>
+
+      <Outlet context={{handleSetUser}}/>
     </>
   )
 }
