be1685c8531302261f456a886d76aabaaa2b994e
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-09-11 18:27:07 +0800

    setup: update blog route, change index element

--- frontend-author/src/routes.jsx
+++ frontend-author/src/routes.jsx
@@ -4,17 +4,19 @@ import { Blogpost } from "./components/BlogPost";
 import { LogIn } from "./components/LogIn";
 import { SignUp } from "./components/SignUp";
 import {NewBlog} from "./components/NewBlog";
+import { UpdateBlog } from "./components/UpdateBlog";
 
 export const routes = [{
     path: "/",
     element: <App />,
     children: [
-        {index: true, element:  <Homepage />},
+        {index: true, element:  <LogIn />},
         {path: "/homepage", element: <Homepage />},
         {path: "/blogpost/:id", element: <Blogpost />},
         {path: "/log-in", element: <LogIn />},
         {path: "/sign-up", element: <SignUp />},
         {path: "/new-blog", element: <NewBlog />},
+        {path: "/update/:id", element: <UpdateBlog />}
     ]
     
 }]
