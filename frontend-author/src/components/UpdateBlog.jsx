197509354a1d9c65a2987bd61044235848391cb8
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-09-11 18:30:58 +0800

    setup, feat: update blog functions

---
+++ frontend-author/src/components/UpdateBlog.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect,useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router";
+
+export const UpdateBlog = () => {
+    const [blog, setBlog] = useState(null);
+    const [title, setTitle]  = useState("");
+    const [error, setError] = useState(null);
+    const editorRef = useRef(null);
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const grabBlog = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/posts/${parseInt(id)}`);
+                setBlog(response.data.content);
+                setTitle(response.data.title);
+                console.log(response);
+            }
+            catch (error) {
+                setError(error);
+            }
+        }
+
+        grabBlog()
+    }, [id]);
+
+    const handleTitleChange  = (e) => {
+        setTitle(e.target.value);
+    }
+
+    const handleUpdateBtn = async () => {
+        try {
+            const response = await axios.put(`http://localhost:3000/posts/${parseInt(id)}`, {
+                title: title,
+                content: editorRef.current.getContent({format: "text"}),
+            })
+
+            console.log(response)
+            if (response.status === 200) {
+                navigate(`/blogpost/${parseInt(id)}`);
+            }
+        }
+        catch (error) {
+            setError(error);
+        }
+    }
+
+    const handleCancelBtn = () => {
+        navigate(`/blogpost/${parseInt(id)}`);
+    } 
+
+    return (
+        <>
+            <h2>Update Blog</h2>
+
+            {error && (
+                <div>{error}</div>
+            )}
+
+            <form action="">
+                <div>
+                    <label htmlFor="updatedTitle">Title: </label>
+                    <input type="text" name="updateTitle" id="updatedTitle" onChange={handleTitleChange} value={title} required/>
+                </div>
+            </form>
+            <Editor
+                apiKey='8eiphr96pnbrf11kcbo70jia5qyh4vc78ukk30cdpl4bzfl0'
+                initialValue={`<p>${blog}</p>`}
+                onInit={(evt, editor) => editorRef.current = editor}
+                init={{
+                height: 500,
+                menubar: false,
+                plugins: [
+                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                    'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                ],
+                toolbar: 'undo redo | blocks | ' +
+                    'bold italic forecolor | alignleft aligncenter ' +
+                    'alignright alignjustify | bullist numlist outdent indent | ' +
+                    'removeformat | help',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                }}
+            />
+
+            <button onClick={handleUpdateBtn}>Update Blog</button>
+            <button onClick={handleCancelBtn}>Cancel</button>
+        </>
+    )
+}
