b9739fb9d24c9c2b3dbfe61e1a0fee273f5fae54
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-09-11 18:23:15 +0800

    feat: log in persistence, log out

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-import { Outlet, Link } from "react-router";
-import { useState } from "react";
+import { Outlet, Link, useNavigate } from "react-router";
+import { useState,  useEffect } from "react";
+import setAuthHeader from "../utils/auth";
 
 const App = () => {
 const [hide, setHide] = useState(false);
-const [user, setUser] = useState({});
+
+const [user, setUser] = useState(() => {
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : {}; 
+});
+
 const [disable, setDisable] = useState(true);
+const navigate = useNavigate();
+
+useEffect(() => {
+  const token = localStorage.getItem("jwt");
+
+  if (token) {
+    setAuthHeader(token);
+  };
+
+}, [])
 
 const handleSetHide = (boolean) => {
   setHide(boolean);
@@ -18,15 +34,26 @@ const handleSetDisable = (boolean) => {
   setDisable(boolean);
 };
 
+const handleLogOutBtn = ()  => {
+  localStorage.removeItem("jwt");
+  localStorage.removeItem("user");
+  setUser({});
+  setDisable(true);
+  navigate("/");
+}
+
   return (
     <>
       <div>
           <nav hidden={hide}>
+              <h1>Welcome to the main page!</h1>
             {Object.keys(user).length > 0 ? (
-              <div>{user.username}</div>
+              <>
+                <div>{user.username}</div>
+                <button onClick={handleLogOutBtn}>Log Out</button>
+              </>
             ) : (
               <>
-                <h1>Welcome to the main page!</h1>
                 <Link to="log-in" onClick={() => handleSetHide(true)}>Log In</Link>
                 <Link to="sign-up" onClick={() => handleSetHide(true)}>Sign Up</Link>
               </>
