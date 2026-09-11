0da43e01a507fc0458cbc1150b8f17977418e097
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2026-09-11 18:26:41 +0800

    feat: log in persistence, log out

--- frontend-author/src/App.jsx
+++ frontend-author/src/App.jsx
@@ -1,10 +1,47 @@
-import { Outlet, Link, useNavigate } from 'react-router'
-import { useState } from 'react'
+import { Outlet, useNavigate } from 'react-router'
+import { useState, useEffect } from 'react'
+import { setAuthHeader } from '../utils/auth';
 import './App.css'
 
 function App() {
-  const [user, setUser] = useState({});
+  const [hidden, setHidden] = useState(() => {
+    const user = localStorage.getItem("user-author");
+    return user ? false : true;
+  });
+
+  const [user, setUser] = useState(() => {
+    const user = localStorage.getItem("user-author");
+    if (user) {
+      return JSON.parse(user);
+    } else  {
+      return {};
+    }
+  });
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt-author");
+
+    localStorage.setItem("user-author", JSON.stringify(user));
+
+    if (token) {
+      setAuthHeader(token);
+    };
+  },[user])
+
+  useEffect(() => {
+    const handleNavigation = () => {
+      if (Object.keys(user).length > 0) {
+        navigate("/homepage")
+      } else {
+        navigate("log-in");
+      }
+    }
+
+    handleNavigation();
+
+  },[user, navigate]);
 
   const handleSetUser = (user) => {
     setUser(user);
@@ -14,24 +51,38 @@ function App() {
     navigate("/new-blog")
   }
 
+  const handleSetHidden = (boolean) => {
+    setHidden(boolean);
+  }
+
+  const handleLogOut = () => {
+    localStorage.removeItem("jwt-author")
+    localStorage.removeItem("user-author");
+    setHidden(true);
+    navigate("/")
+  }
+
   return (
     <>
-    <nav>
+    <nav hidden={hidden}>
       <h1>Main page</h1>
-      {Object.keys(user).length > 0 ? (
-        <div>{user.username}</div>
-      ) : (
-        <div>
-        <Link to="log-in">Log in</Link>
-        <Link to="sign-up">Sign up</Link>
-      </div>
+      
+      {user && (
+        Object.keys(user).length > 0 && (
+          <>
+            <div>{user.username}</div>
+            <button onClick={handleLogOut}>Log Out</button>
+          </>
+            
+      )
       )}
+
       <div>
         <button onClick={handleNewBlogNav}>Create New Blog!</button>
       </div>
     </nav>
 
-      <Outlet context={{handleSetUser}}/>
+      <Outlet context={{handleSetUser, handleSetHidden}}/>
     </>
   )
 }
